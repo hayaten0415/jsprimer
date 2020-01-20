b93603be3f4adafb6e15848e201697cac3b8ed57
Hayashi Hironori <hayaten415@gmail.com>
2020-01-20 22:10:21 +0900

    read File only nodecli app

---
+++ nodecli/main.js
@@ -0,0 +1,19 @@
+// commanderモジュールをprogramとしてインポートする
+const program = require("commander");
+// fsモジュールをfsオブジェクトとしてインポートする
+const fs = require("fs");
+
+// コマンドライン引数をcommanderでパースする
+program.parse(process.argv);
+const filePath = program.args[0];
+
+// ファイルを非同期で読み込む
+fs.readFile(filePath, { encoding: "utf8"}, (err, file) => {
+    if (err) {
+        console.error(err.message);
+        // 終了ステータス 1（一般的なエラー）としてプロセスを終了する
+        process.exit(1);
+        return;
+    }
+    console.log(file);
+});
